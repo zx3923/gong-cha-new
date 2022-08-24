d61aecd09f9dbb13047fa218288af760f9610320
zx3923 <seop3923@gmail.com>
2022-08-24 22:53:25 +0900

    step2 완료

--- src/component/Step.js
+++ src/component/Step.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
+import Step3 from "./Step3";
 
 const Step = () => {
   return (
     <>
       <Step1 />
       <Step2 />
+      <Step3 />
     </>
   );
 };
---
+++ src/component/Step3.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from "./Step3.module.css";
+
+const Step3 = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default Step3;
